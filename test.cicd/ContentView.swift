df1620b560c1debce5e8b34780da5d24d4205c49
Johnny Peterson <johnny.peterson@wgu.edu>
2020-08-11 13:55:27 -0600

    Initial commit

---
+++ test.cicd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test.cicd
+//
+//  Created by Johnny Peterson on 8/11/20.
+//  Copyright © 2020 Johnny Peterson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
